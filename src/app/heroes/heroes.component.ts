d208ac432d6beaaa4a8c096968f8d7cea5528bfd
David Koncsol <davekoncsol@gmail.com>
2019-12-26 10:57:27 -0800

    updated styling

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -6,19 +6,24 @@ import { HEROES } from '../mock-heroes';
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
-  
+
 })
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
+
   };
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   constructor() { }
 
   ngOnInit() {
-    
+
   }
 
 }
